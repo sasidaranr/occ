0504b48d6bb063c8d2d3e39191df9db21d66d208
sasidaranr <sasidaran.rajaram@listertechnologies.com>
2016-04-08 18:47:29 +0530

    Initial commit with a sample extension and files

---
+++ HelloWorldExtension/widget/helloWorld1/js/helloWorld1.js
@@ -0,0 +1,13 @@
+define(
+  [],
+  function () {
+      "use strict";
+      return {
+        onLoad : function (widget) {
+        },
+        beforeAppear : function (page) {
+          alert("hello world1");
+        }
+      };
+  }
+);
